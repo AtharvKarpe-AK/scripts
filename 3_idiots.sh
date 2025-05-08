d37bc00cb4986f51334fa45ec830c214e68d62d5
Ubuntu <ubuntu@ip-172-31-46-102.eu-west-1.compute.internal>
2025-05-08 14:54:30 +0000

    Add script to be tracked

---
+++ 3_idiots.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+hero="rancho"
+villain="Virus"
+
+echo "ye movie ks hero h $hero"
+
+echo "ye movie ka villain h $villain"
+
+echo "My current user is $USER"
+
+read -p "rancho ka pura naam kya tha: " fullname
+
+echo "rancho ka pura naam $fullname tha"
+
+echo "Movie ka naam $0 tha"
+echo "first idiot is $1"
+echo "first idiot is $2"
+echo "first idiot is $3"
+
+echo "hence all idiot are $@"
+
+echo "total: $#"
